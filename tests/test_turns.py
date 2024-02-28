1e051cbfa42d66cc2f00b1bf1f0e22de27a47192
Federico Bogado <ficobogado@gmail.com>
2024-02-28 02:04:18 -0300

    Using datetime class in turns

--- tests/test_turns.py
+++ tests/test_turns.py
@@ -1,10 +1,11 @@
 import pytest
+from datetime import datetime
 from turns_app.turns import turn_id_generator, Turn
 
 
 def test_turn_id_generator():
-
-    result = turn_id_generator('26.02.2024', '08.00', 'office_id')
+    start_time = datetime(2024, 2, 26, 8, 0)
+    result = turn_id_generator(start_time, 'office_id')
     assert result == 'TURN-26.02.2024-08.00-office_id'
 
 
@@ -12,17 +13,13 @@ def test_turn_generation():
 
     turn = Turn(
         idx='TURN-26.02.2024-08.00-office_id',
-        date='26.02.2024',
-        time='08.00',
-        day='Monday',
-        duration=60,
+        start_time=datetime(2024, 2, 26, 8, 0),
+        end_time=datetime(2024, 2, 26, 10, 00),
         user_id='USER_01',
         office_id='OFF_01'
     )
     assert turn.idx == 'TURN-26.02.2024-08.00-office_id'
-    assert turn.date == '26.02.2024'
-    assert turn.time == '08.00'
-    assert turn.day == 'Monday'
-    assert turn.duration == 60
+    assert turn.start_time == datetime(2024, 2, 26, 8, 0)
+    assert turn.end_time == datetime(2024, 2, 26, 10, 00)
     assert turn.user_id == 'USER_01'
     assert turn.office_id == 'OFF_01'
--- turns_app/turns.py
+++ turns_app/turns.py
@@ -1,23 +1,22 @@
+from datetime import datetime
 from dataclasses import dataclass
 
 from turns_app.utils.dataclass_utils import BaseDataclass
 
 
-def turn_id_generator(date: str, time: str, office_id: str):
+def turn_id_generator(start_time: datetime, office_id: str):
     """Generate unique IDs for turns"""
-    prefix = 'TURN-'
-    return f"{prefix}{date}-{time}-{office_id}"
+    date = start_time.date().strftime("%d.%m.%Y")
+    time = start_time.time().strftime("%H.%M")
+    return f"TURN-{date}-{time}-{office_id}"
 
 
 @dataclass
 class Turn(BaseDataclass):
-    idx: str        # Unique identifier
+    idx: str              # Unique identifier
 
-    # TODO: Implement better date
-    date: str       # DD.MM.YYYY
-    time: str       # HH:MM
-    day: str
-    duration: int   # minutes
+    start_time: datetime  # Turn start time
+    end_time: datetime    # Turn end time
 
-    user_id: str    # User unique identifier
-    office_id: str  # Office unique identifier
+    user_id: str          # User unique identifier
+    office_id: str        # Office unique identifier
